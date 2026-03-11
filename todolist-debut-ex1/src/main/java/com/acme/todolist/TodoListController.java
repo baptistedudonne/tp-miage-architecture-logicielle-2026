917cf318f8d10c5081d66e41ac9795f1fada11d4
Baptiste D <bapt.14@hotmail.com>
2026-03-11 14:54:42 +0100

    ✨ ajout du Inject sur le contructeur de TodoListController

--- todolist-debut-ex1/src/main/java/com/acme/todolist/TodoListController.java
+++ todolist-debut-ex1/src/main/java/com/acme/todolist/TodoListController.java
@@ -1,16 +1,13 @@
 package com.acme.todolist;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Inject;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Le controlleur Spring MVC qui expose les endpoints REST
@@ -24,6 +21,7 @@ public class TodoListController {
 	private static final String LATE = "[LATE!]";
 	private TodoItemRepository todoItemRepository;
 
+	@Inject
 	public TodoListController(TodoItemRepository todoItemRepository) {
 		super();
 		this.todoItemRepository = todoItemRepository;
@@ -36,8 +34,7 @@ public class TodoListController {
 	@PostMapping("/todos")
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public void createTodoItem(@RequestBody TodoItem todoItem) {
-		// Code à compléter
-		// ...
+		todoItemRepository.save(todoItem);
 	}
 
 	@GetMapping("/todos")
