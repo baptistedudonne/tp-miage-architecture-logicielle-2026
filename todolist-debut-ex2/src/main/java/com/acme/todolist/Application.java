a6f42dcbacba7ba9178ec8cc4962aa904a41bc52
Baptiste D <bapt.14@hotmail.com>
2026-03-11 15:51:19 +0100

    ✨ exercice 2

---
+++ todolist-debut-ex2/src/main/java/com/acme/todolist/Application.java
@@ -0,0 +1,13 @@
+package com.acme.todolist.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.acme.todolist")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
